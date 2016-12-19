53996672ee62c43132cd127651cb914bb3f524c4
Viacheslav <a05@ukr.net>
2016-12-19 22:06:10 +0200

    New store structure

---
+++ src/redux/initState.js
@@ -0,0 +1,21 @@
+const initialState = {
+  "lang": "en",
+  "isAddingCity": false,
+  "places": [
+    {
+      "place": "Current",
+      "placeID": 0,
+      "lat": 46.489631,
+      "lng": 30.728830,
+      "weather": ""
+    },
+    {
+      "place": "CityName1",
+      "placeID": 1,
+      "lat": 50.450137,
+      "lng": 30.523611,
+      "weather": ""
+    }
+  ]
+}
+export default initialState
