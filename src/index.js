a0f0834c236459234549c24d2a277ede5366f8f1
Viacheslav <a05@ukr.net>
2017-01-16 15:29:16 +0200

    Intl polifill final implementation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import Root from '_containers/Root'
-import shim from '_utils/intlPolyfill'
 
-const run = () => {
-  render(
-    <Root />,
-    document.getElementById('root')
-  )
-}
-
-shim( run )
+render(
+  <Root />,
+  document.getElementById('root')
+)
